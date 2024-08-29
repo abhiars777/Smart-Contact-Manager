94be4ebeee25725f3440775ea2f486f8559c2785
abhiars777 <abhiars777@gmail.com>
2024-08-30 01:31:09 +0530

    Sql setup

---
+++ src/main/java/com/scm/entities/User.java
@@ -0,0 +1,49 @@
+package com.scm.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    private int userId;
+    @Column(nullable = false)
+    private String name;
+    @Column( nullable = false, unique = true)
+    private String email;
+    private String password;
+    @Column( length = 1000)
+    private String about;
+    private String profilePic;
+    private String phoneNumber;
+
+    private boolean enabled = false;
+    private boolean phoneVerified = false;
+    private boolean emailVerified = false;
+
+    private Providers provider = Providers.SELF;
+    private String providerUserId;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY ,orphanRemoval = true)
+    private List<Contact> contacts = new ArrayList<>();
+    
+}
