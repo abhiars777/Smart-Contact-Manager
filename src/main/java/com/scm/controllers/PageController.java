554fa4855f2a31f925436d89060f618c2e5b6145
abhiars777 <abhiars777@gmail.com>
2024-08-30 21:00:54 +0530

    data from form to controller

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,9 +3,12 @@ package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+import com.scm.forms.UserForm;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -41,13 +44,23 @@ public class PageController {
     }
 
     @GetMapping("/register")
-    public String registerPage() {
+    public String registerPage(Model model) {
+        UserForm userForm = new UserForm();
+        // userForm.setName("Abhishek");
+        // userForm.setEmail("abhi");
+        // userForm.setPassword("pass");
+        // userForm.setPhoneNumber("1234");
+        // userForm.setAbout("Awesome");
+        model.addAttribute("userForm", userForm);
         return "register";
     }
     
-    
-    
-    
-    
+
+    @PostMapping("/do-register")
+    public String registrationprocess(@ModelAttribute UserForm userForm) {
+        System.out.println("here im....................");
+        System.out.println(userForm);
+        return "redirect:/register";
+    }
 
 }
